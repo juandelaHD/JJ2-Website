0cda318918f422b5610efe0dc4aae35f425a3ecf
juandelaHD <jdelacruz@fi.uba.ar>
2024-07-04 00:01:29 -0300

    [feat] - "Scrap Latest Game Version" - falta dividirlo como "backend" - falta vincular la logica al botón

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,173 +1,42 @@
-var sBrowser, sUsrAg = navigator.userAgent;
-let parallaxContainer = document.querySelector('.parallax-container');
+const puppeteer = require('puppeteer');
 
-if (sUsrAg.indexOf("Firefox") > -1) {
-    sBrowser = "Firefox";
+const URL_REPO = 'https://github.com/Taller-de-Programacion-1-1C2024-GRUPO1/TP-Jazz-Jackrabbit-2/releases/';
+const TAG = 'a'
+const ZIP = 'zip'
+
+async function setDriver() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(URL_REPO, { waitUntil: 'networkidle2' });
+    return { browser, page };
 }
 
-if (sBrowser == "Firefox") {
-    parallaxContainer.style.backgroundColor = "#171717";
+async function getLatestVersion() {
+    const { browser, page } = await setDriver();
+    // loop that waits for the first release to load in the github page (releases are loaded asynchronously)
+    const latestVersion = await page.evaluate(() => {
+        const links = document.querySelectorAll(TAG);
+        for (let link of links) {
+            if (link.href.includes(ZIP)) {
+                return link.href;
+            }
+        }
+        return null;
+    });
+    await browser.close();
+    return latestVersion;
 }
 
-let wallpaperCarousel = document.querySelector('#wallpaper-carousel');
-let dotContainer = document.querySelector('#dot-container');
-
-let wallpapers = [];
-let wallpaperIndex = 1;
-
-// Get Path to Wallpaper
-const webpPath = 'assets/images/webp/1080/';
-const jpgPath = 'assets/images/jpg/1080/';
-const pngPath = 'assets/images/png/1080/';
-
-// Create Picture Element
-const displayWallpaper = (src, alt) => {
-    // Parent Element
-    let picture = document.createElement('picture');
-    picture.className = 'fade';
-    // Child Element
-    let webpSource = document.createElement('source');
-    let jpgSource = document.createElement('source');
-    let pngSource = document.createElement('source');
-    let image = document.createElement('img');
-    // Attribute type
-    webpSource.type = 'image/webp';
-    jpgSource.type = 'image/jpeg';
-    pngSource.type = 'image/png';
-    // Attribute srcset
-    webpSource.srcset = `${webpPath}${src}.webp`;
-    jpgSource.srcset = `${jpgPath}${src}.jpg`;
-    pngSource.srcset = `${pngPath}${src}.png`;
-    // Attribute img
-    image.className = 'img-resize';
-    image.src = `${webpPath}${src}.webp`;
-    image.alt = alt;
-    image.loading = "lazy";
-    // Append Child Element
-    picture.append(webpSource, jpgSource, pngSource, image);
-    // Return Picture Element
-    return picture;
-}
-
-const displayDot = (wallpaperLength) => {
-    let dots = [];
-    for (let index = 0; index < wallpaperLength; index++) {
-        //Create Dot Element
-        let dot = document.createElement('span');
-        dot.className = 'dot hover';
-        dot.addEventListener('click', () => { currentWallpaper(index + 1); });
-        dots.push(dot);
-    }
-    return dots;
-}
-
-for (let index = 1; index <= 7; index++) {
-    wallpapers.push(displayWallpaper(`image${index}`, `wallpaper${index}`));
-}
-
-wallpaperCarousel.append(...wallpapers);
-dotContainer.append(...displayDot(wallpapers.length));
-
-function showWallpapers(index) {
-    // let i;
-    let wallpapers = document.querySelector('#wallpaper-carousel').children;
-    let dots = document.querySelector('#dot-container').children;
-
-    if (index > wallpapers.length) { wallpaperIndex = 1 }
-    if (index < 1) { wallpaperIndex = wallpapers.length }
-
-    for (let i = 0; i < wallpapers.length; i++) {
-        wallpapers[i].style.display = "none";
-    }
-
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' dot-active', '');
-    }
-
-    wallpapers[wallpaperIndex - 1].style.display = "block";
-    dots[wallpaperIndex - 1].className += ' dot-active';
-}
-
-showWallpapers(wallpaperIndex);
-
-function currentWallpaper(index) {
-    showWallpapers(wallpaperIndex = index);
-}
-
-function nextWallpaper(index) {
-    showWallpapers(wallpaperIndex += index);
-}
-
-const videoYoutubeContainer = document.querySelector('#video-youtube-container');
-
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCZS2K8ZsUmujTuj3cNMyBSA&part=snippet%2Cid&order=date&maxResults=18';
-
-/* Fetching data from YouTube API. */
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '426e606a38msh2632a8b4514886dp176946jsn1ac50e7bfbd4',
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-};
-
-async function fetchData(urlApi) {
-    const response = await fetch(urlApi, options);
-    const data = await response.json();
-    return data;
-}
-
-function videoYoutube(thumbnailUrl, title, videoId) {
-    // Video Container
-    let videoYoutube = document.createElement('div');
-    videoYoutube.className = 'video-youtube';
-
-    //Thumbnail Container
-    let thumbnailContainer = document.createElement('div');
-    thumbnailContainer.className = 'thumbnail-container';
-
-    //Icon Play
-    let iconPlay = document.createElement('img');
-    iconPlay.src = 'assets/svg/icon-youtube.svg';
-    iconPlay.alt = 'play';
-    iconPlay.className = 'play-icon';
-
-    //Thumbnail
-    let thumbnail = document.createElement('img');
-    thumbnail.src = thumbnailUrl;
-    thumbnail.alt = title;
-    thumbnail.className = 'thumbnail';
-    thumbnailContainer.append(iconPlay, thumbnail);
-
-    //Title
-    let titleContainer = document.createElement('div');
-    titleContainer.className = 'title-container';
-    let h3 = document.createElement('h3');
-    let span = document.createElement('span');
-    span.textContent = title;
-    span.href = `https://www.youtube.com/watch?v=${videoId}`;
-    span.target = '_blank';
-    span.rel = 'noopener noreferrer';
-    h3.append(span);
-    titleContainer.append(h3);
-
-    videoYoutube.append(thumbnailContainer, titleContainer);
-    return videoYoutube;
-}
-
-(async () => {
+document.getElementById('install').addEventListener('click', async () => {
     try {
-        const videos = await fetchData(API);
-        const items = videos.items.reverse();
-        let videosYoutube = items.map(video => videoYoutube
-            (
-                video.snippet.thumbnails.medium.url,
-                video.snippet.title,
-                video.id.videoId
-            )
-        );
-        videoYoutubeContainer.append(...videosYoutube);
+        const downloadLink = await getLatestVersion();
+        if (downloadLink) {
+            window.location.href = downloadLink; // Esto redirige el navegador al enlace de descarga
+        } else {
+            alert('No se encontró la última versión.');
+        }
     } catch (error) {
-        console.error(error);
+        console.error('Error al obtener la última versión:', error);
+        alert('Hubo un error al intentar obtener la última versión.');
     }
-})()
+})
